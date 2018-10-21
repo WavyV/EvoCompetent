e545d12fe649eb3e7e524ddd90b09e61a98a889a
Mirbruin <ditisnietmijnechtemailadres@gmail.com>
2018-10-21 17:40:27 +0200

    data processing and analysis python notebooks

--- Islands/multiple.sh
+++ Islands/multiple.sh
@@ -2,7 +2,7 @@
 
 $(javac -cp contest.jar player51.java)
 
-for i in {0..1};
+for i in {0..99};
 do
-  echo $(java -jar testrun.jar -submission=player51 -evaluation=SchaffersEvaluation -seed=$i >~/Desktop/EvoCompetent/Islands/results_schaffers/results_seed$i.txt)
+  echo $(java -jar testrun.jar -submission=player51 -evaluation=SchaffersEvaluation -seed=$i >~/documents/GitHub/EvoCompetent/Islands/Results_Schaffers/results_seed$i.txt)
 done
